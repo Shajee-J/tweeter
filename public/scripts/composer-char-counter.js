9f0b0ee909d718d99d523aa44fbe3b3642281078
Shajee-j <Shajeejm@gmail.com>
2023-07-19 23:17:22 -0400

    created fake tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,43 @@
-$(document).ready(function() {
-  let counter = document.querySelector(".counter");
+$(document).ready(function () {
 
-  const textArea = document.querySelector("#tweet-text");
-  
-  textArea.addEventListener("input", function() {
+  // using jquery:
 
-    const typedCharacters = textArea.value.length;
-   
-    counter.textContent = 140 - typedCharacters;
+  let counter = $(".counter");
 
-    if (counter.textContent < 0) {
-      counter.classList.add("overLimit")
+  const textArea = $("#tweet-text");
+
+  $("#tweet-text").on("input", function () {
+    
+    const typedCharacters = textArea.val().length;
+    
+    counter.text( 140 - typedCharacters);
+    
+    if (counter.text() < 0) {
+      counter.addClass("overLimit")
     }
-
-    if (counter.textContent > 0) {
-      counter.classList.remove("overLimit")
+    
+    if (counter.text() > 0) {
+      counter.removeClass("overLimit")
     }
-  })
+  });
+
+// using vanilla js:
+
+// let counter = document.querySelector(".counter")
+// let textArea = document.querySelector("#tweet-text")
+
+//   textArea.addEventListener("input", function() {
+//       const typedCharacters = textArea.value.length;
+
+//       counter.textContent = 140 - typedCharacters;
+
+//       if (counter.textContent < 0) {
+//         counter.classList.add("overLimit")
+//       }
+
+//       if (counter.textContent > 0) {
+//         counter.classList.remove("overLimit")
+//       }
+//     })
 });
 
