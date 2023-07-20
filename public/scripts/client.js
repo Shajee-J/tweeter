d3b3d01b827c89095716a6add7f455fadf948a33
Shajee-j <Shajeejm@gmail.com>
2023-07-20 13:03:53 -0400

    implement dynamic tweets via client.js & bug fix

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,40 +1,65 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function () {
 
-$(document).ready(function () { 
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
-  const createTweetElement = () => {
+
+  createTweetElement = (item) => {
+
     const $tweet = $(`
-      <article id="demoTweets">
-        <header>
-        <div><i class="fa-solid fa-user"></i> Rhoda Jacobs</div> 
-        <div id="At">@Mrs. Jacobs</div>
-        </header>
-        <div id="demoTweetsText"><p>Hello World!</p></div>    
-        <footer id="demoTweetsFooter"> 
-        <div>10 days ago</div> 
-        <div id="icons" >
-        <i class="fa-solid fa-flag"></i> 
-        <i class="fa-solid fa-retweet"></i> 
-        <i class="fa-solid fa-heart"></i>
-        </div>
-        </footer> 
-      </article>
-  `);
+        <article id="demoTweets">
+          <header>
+          <div><img src=${item.user.avatars}></i> ${item.user.name}</div> 
+          <div id="At">${item.user.handle}</div>
+          </header>
+          <div id="demoTweetsText"><p>${item.content.text}</p></div>    
+          <footer id="demoTweetsFooter"> 
+          <div>${item.created_at}</div> 
+          <div id="icons" >
+          <i class="fa-solid fa-flag"></i> 
+          <i class="fa-solid fa-retweet"></i> 
+          <i class="fa-solid fa-heart"></i>
+          </div>
+          </footer> 
+        </article>
+    `);
     return $tweet;
   };
 
 
+  const renderTweets = function (data) {
+    for (let item of data) {
+      $('#tweets-container').append(createTweetElement(item));
+    }
+  };
 
-const $tweet = createTweetElement();
+  renderTweets(data)
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$(document).ready( () => {
-    $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-});
+
+
+
 
 });
