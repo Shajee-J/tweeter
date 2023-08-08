403098d8511e1c98a547a2b8e6258611129bbfe3
Shajee-j <Shajeejm@gmail.com>
2023-08-08 14:55:50 -0400

    implement onScroll err class empty msg

--- public/scripts/scrollbar-err-remover.js
+++ public/scripts/scrollbar-err-remover.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  // will get rid off error msg before it overlaps with the fixed nav
   
 $(window).on('scroll', function() {
  if (document.body.scrollTop > 360 || document.documentElement.scrollTop > 650){
